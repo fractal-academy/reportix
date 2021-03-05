c937c4607e9647b0cfa953dac5287b7379dffae3
okrdima <okrdima@gmail.com>
2021-03-05 15:10:57 +0200

    remove component UserSimpleView from App.js

--- src/app/index.js
+++ src/app/index.js
@@ -2,12 +2,11 @@ import { ThemeProvider } from '@qonsoll/react-design'
 import Theme from '../config/theme'
 import '../config/root.scss'
 import 'antd/dist/antd.css'
-import { UserSimpleView } from 'domains/user/components/views/UserSimpleView'
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <UserSimpleView />
+      <div>Hello app</div>
     </ThemeProvider>
   )
 }
