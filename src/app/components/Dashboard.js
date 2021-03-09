70bf40568a6eeace7a4e3d8f72944c56bee1d369
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-03-09 17:47:19 +0200

    added userProjectsSimpleView component

--- src/app/components/Dashboard.js
+++ src/app/components/Dashboard.js
@@ -3,16 +3,19 @@ import { auth } from '../services/Firebase/firebase'
 import Title from 'antd/lib/typography/Title'
 import { useSession } from '../context/SesionContext'
 import { Box } from '@qonsoll/react-design'
+import PageWrapper from 'components/PageWrapper'
 
 const Dashboard = () => {
   const { currentUser } = useSession()
 
   return (
-    <Box p={4}>
-      <Title>Dashboard</Title>
-      <Title level={4}>Current user: {currentUser.email}</Title>
-      <Button onClick={() => auth.signOut()}>Sign out</Button>
-    </Box>
+    <PageWrapper>
+      <Box p={2}>
+        <Title>Dashboard</Title>
+        <Title level={4}>Current user: {currentUser.email}</Title>
+        <Button onClick={() => auth.signOut()}>Sign out</Button>
+      </Box>
+    </PageWrapper>
   )
 }
 
