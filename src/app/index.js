518ca34a938beff7c926d7834374461a08972595
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-03-09 13:01:15 +0200

    deleted repeated index

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Login } from 'app/services/Auth'
+import { SignUp } from 'app/services/Auth'
+import Dashboard from 'components'
+import PrivateRoute from 'components/PrivateRoute'
+import { AuthProvider } from './context/SesionContext/useSession'
 import { ThemeProvider } from '@qonsoll/react-design'
-import Theme from '../config/theme'
-import '../config/root.scss'
-import 'antd/dist/antd.css'
-import { Box } from '@qonsoll/react-design'
+import Theme from 'app/config/theme'
+import 'app/config/root.scss'
 
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
-      <Box>Hello</Box>
+      <AuthProvider>
+        <Router>
+          <PrivateRoute exact path="/" component={Dashboard} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </Router>
+      </AuthProvider>
     </ThemeProvider>
   )
 }
