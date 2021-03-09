d4500652c1582f4bc7f34d88fd6bd7f98fa0ba51
sashka2131 <72075757+sashka2131@users.noreply.github.com>
2021-03-09 18:37:30 +0200

    Update UserProjectSimpleView.template.js
    removed unnecessary comments

--- src/app/domains/project/components/views/UserProjectSimpleView/UserProjectSimpleView.template.js
+++ src/app/domains/project/components/views/UserProjectSimpleView/UserProjectSimpleView.template.js
@@ -44,17 +44,3 @@ UserProjectSimpleView.defaultProps = {
 }
 
 export default UserProjectSimpleView
-
-// hints To insert project to any components
-
-// const projectsArr = [0, 1, 3]
-//
-// <Row>
-// {projectsArr.map((item, index) => (
-//       <Fragment key={index}>
-//         <Col cw={4} mb={3}>
-//           <UserProjectSimpleView />
-//         </Col>
-//       </Fragment>
-//   ))}
-// </Row>
