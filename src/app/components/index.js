0b42f60bba2e63ac837733b339887fbcfe16badc
sashka2131 <uhucbeucg@gmail.com@gmail.com>
2021-03-11 18:25:55 +0200

    revrited PAGES constant and revrite map for page side menu, remove Custom Menu component

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,5 +1,4 @@
 import PageWrapper from './PageWrapper'
 import Header from './Header'
-import CustomMenu from './Menu'
 import Dashboard from './Dashboard'
-export { PageWrapper, Header, CustomMenu, Dashboard }
+export { PageWrapper, Header, Dashboard }
--- src/app/constants/pages.js
+++ src/app/constants/pages.js
@@ -1,11 +1,11 @@
 import { ROUTES_PATHS } from 'app/constants/'
 
-const PAGES = {
-  'Company dashboard': ROUTES_PATHS.COMPANIES_SHOW,
-  Calendar: ROUTES_PATHS.CALENDAR_SHOW,
-  Projects: ROUTES_PATHS.PROJECTS_ALL,
-  Reports: ROUTES_PATHS.REPORTS_ALL,
-  Statistic: ROUTES_PATHS.STATISTICS_ALL
-}
+const PAGES = [
+  { text: 'Company dashboard', path: ROUTES_PATHS.COMPANIES_SHOW },
+  { text: 'Calendar', path: ROUTES_PATHS.CALENDAR_SHOW },
+  { text: 'Projects', path: ROUTES_PATHS.PROJECTS_ALL },
+  { text: 'Reports', path: ROUTES_PATHS.REPORTS_ALL },
+  { text: 'Statistic', path: ROUTES_PATHS.STATISTICS_ALL }
+]
 
 export default PAGES
