0cac8821363e807dafa4f10ae694c5296e9b69e3
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-03-11 15:34:32 +0200

    created Invoice Advanced View

---
+++ src/app/domains/Invoice/components/view/InvoiceAdvancedView/InvoiceAdvancedView.js
@@ -0,0 +1,6 @@
+import { Box } from '@qonsoll/react-design'
+
+const InvoiceAdvancedView = () => {
+  return <Box></Box>
+}
+export default InvoiceAdvancedView
---
+++ src/app/domains/Invoice/components/view/InvoiceAdvancedView/index.js
@@ -0,0 +1,2 @@
+import InvoiceAdvancedView from 'domains/Invoice/components/view/InvoiceAdvancedView/InvoiceAdvancedView'
+export default InvoiceAdvancedView
