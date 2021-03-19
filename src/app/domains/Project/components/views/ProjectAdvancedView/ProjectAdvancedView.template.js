81f2658deef4ade70cddf5fa8104f56282daf77a
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-03-19 11:30:46 +0200

    added delete button

--- src/app/domains/Project/components/views/ProjectAdvancedView/ProjectAdvancedView.template.js
+++ src/app/domains/Project/components/views/ProjectAdvancedView/ProjectAdvancedView.template.js
@@ -1,14 +1,34 @@
 import { Box, Col, Row } from '@qonsoll/react-design'
 import Text from 'antd/lib/typography/Text'
 import { UserGroupView } from 'domains/User/components/views'
-import { Button, Card } from 'antd'
+import { Button, Card, Popconfirm } from 'antd'
 import { EditOutlined } from '@ant-design/icons'
 import { ROUTES_PATHS } from '../../../../../constants'
 import { useHistory } from 'react-router-dom'
+import { useState } from 'react'
 
 const ProjectAdvancedView = (props) => {
   const { company, project, tasks, startDate, deadline } = props
   const history = useHistory()
+  const [visible, setVisible] = useState(false)
+  const [confirmLoading, setConfirmLoading] = useState(false)
+
+  const showPopconfirm = () => {
+    setVisible(true)
+  }
+
+  const handleOk = () => {
+    setConfirmLoading(true)
+    setTimeout(() => {
+      setVisible(false)
+      setConfirmLoading(false)
+    }, 2000)
+  }
+
+  const handleCancel = () => {
+    console.log('Clicked cancel button')
+    setVisible(false)
+  }
 
   return (
     <Card hoverable>
@@ -34,7 +54,7 @@ const ProjectAdvancedView = (props) => {
             </Col>
           </Row>
         </Col>
-        <Col cw="auto">
+        <Col cw="auto" v={'end'}>
           <Box>
             <Text>Project start date: {startDate}</Text>
           </Box>
@@ -43,14 +63,30 @@ const ProjectAdvancedView = (props) => {
           </Box>
         </Col>
         <Col cw="auto">
-          <Button
-            type="primary"
-            icon={<EditOutlined />}
-            onClick={() => {
-              history.push(ROUTES_PATHS.PROJECT_EDIT)
-            }}>
-            Edit
-          </Button>
+          <Row noGutters>
+            <Col mr={2}>
+              <Button
+                type="primary"
+                icon={<EditOutlined />}
+                onClick={() => {
+                  history.push(ROUTES_PATHS.PROJECT_EDIT)
+                }}>
+                Edit
+              </Button>
+            </Col>
+            <Col>
+              <Popconfirm
+                title="Delete this project?"
+                visible={visible}
+                onConfirm={handleOk}
+                okButtonProps={{ loading: confirmLoading }}
+                onCancel={handleCancel}>
+                <Button danger onClick={showPopconfirm}>
+                  Delete
+                </Button>
+              </Popconfirm>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Card>
