0e24c52158c59a65c2935eb4daaf890c9f891673
okrdima <okrdima@gmail.com>
2021-03-22 10:51:15 +0200

    add Edit User

--- src/app/domains/User/routes/UsersEdit/UsersEdit.layout.js
+++ src/app/domains/User/routes/UsersEdit/UsersEdit.layout.js
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { Button, Modal, Typography, Form, message } from 'antd'
 import { UserSimpleForm } from 'domains/User/components/forms'
 import { EditOutlined } from '@ant-design/icons'
+import { updateData } from 'services/Firestore'
+import { useParams } from 'react-router-dom'
+import COLLECTIONS from 'constants/collection'
 
 message.config({ maxCount: 2 })
 
@@ -9,22 +12,28 @@ const { Title } = Typography
 const UsersEdit = () => {
   const [visible, setVisible] = useState(false)
   const [loading, setLoading] = useState(false)
+  const { id } = useParams()
   const [form] = Form.useForm()
   const handleCancel = () => {
     setVisible(false)
     setLoading(false)
     form.resetFields()
   }
-  const onEditUser = async (value) => {
-    console.log(value)
+  const onEditUser = async (data) => {
+    console.log(data)
     setLoading(true)
-    // try {
-    //   await auth.sendPasswordResetEmail(value.email)
-    //   setVisible(false)
-    //   form.resetFields()
-    // } catch (e) {
-    //   message.error("Can't find this email.")
-    // }
+    try {
+      await updateData(COLLECTIONS.USERS, id, {
+        firstName: data.firstName,
+        surname: data.surname,
+        phone: data.phone,
+        photoURL: data?.photoURL[1]
+      })
+      setVisible(false)
+      form.resetFields()
+    } catch (e) {
+      message.error("Can't find this email.")
+    }
 
     setLoading(false)
     setVisible(false)
@@ -46,7 +55,11 @@ const UsersEdit = () => {
           <Button key="back" onClick={handleCancel}>
             Cancel
           </Button>,
-          <Button key="submit" type="primary" onClick={() => form.submit()}>
+          <Button
+            key="submit"
+            type="primary"
+            onClick={() => form.submit()}
+            loading={loading}>
             Edit
           </Button>
         ]}>
