0faa51c4cc9650a40b5ce7fd6cace5e2804f175d
okrdima <okrdima@gmail.com>
2021-04-01 02:32:09 +0300

    add delete event

--- src/app/domains/Calendar/components/views/Calendar/Calendar.template.js
+++ src/app/domains/Calendar/components/views/Calendar/Calendar.template.js
@@ -3,21 +3,32 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
 import interactionPlugin from '@fullcalendar/interaction'
-import { Button, Typography } from 'antd'
+import { Button, Typography, message, Popconfirm } from 'antd'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
-import { getCollectionRef } from 'services/Firestore'
+import { deleteData, getCollectionRef, updateData } from 'services/Firestore'
 import COLLECTIONS from 'constants/collection'
 import { Spinner } from 'components/Spinner'
 import STATUS from 'constants/status'
-import { Box } from '@qonsoll/react-design'
+import { Box, Col } from '@qonsoll/react-design'
 import { DeleteOutlined } from '@ant-design/icons'
+import { useState } from 'react'
 const { Text } = Typography
 
 const CalendarAdvancedView = () => {
   const [events, loading] = useCollectionData(
-    getCollectionRef(COLLECTIONS.LEAVE_DAYS)
+    getCollectionRef(COLLECTIONS.LEAVE_DAYS),
+    { idField: 'id' }
   )
-
+  const updateEventData = async (event) => {
+    try {
+      await updateData(COLLECTIONS.LEAVE_DAYS, event.event._def.publicId, {
+        start: new Date(event.event._instance.range.start),
+        end: new Date(event.event._instance.range.end)
+      })
+    } catch (e) {
+      message.error('Can`t move event')
+    }
+  }
   if (!events || loading) {
     return <Spinner />
   }
@@ -36,9 +47,9 @@ const CalendarAdvancedView = () => {
     <FullCalendar
       navLinks
       firstDay={1}
-      eventDrop={(e) => console.log(e)}
+      eventDrop={updateEventData}
       plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
-      eventContent={renderEventContent}
+      eventContent={(e) => <RenderEventContent {...e} />}
       initialView="dayGridMonth"
       selectable
       selectMirror
@@ -56,7 +67,19 @@ const CalendarAdvancedView = () => {
   )
 }
 
-const renderEventContent = (eventInfo) => {
+export default CalendarAdvancedView
+
+const RenderEventContent = (eventInfo) => {
+  const [visible, setVisible] = useState(false)
+  console.log(eventInfo.event._def.publicId)
+  const handleOk = async (data) => {
+    try {
+      await deleteData(COLLECTIONS.LEAVE_DAYS, eventInfo.event._def.publicId)
+    } catch (error) {
+      message.error('Can`t delete event')
+    }
+    setVisible(!visible)
+  }
   return (
     <Box display="flex" justifyContent="space-between" alignItems="center">
       <Text
@@ -67,9 +90,25 @@ const renderEventContent = (eventInfo) => {
         }}>
         {eventInfo.event.title}
       </Text>
-      <Button size="small" type="text" danger icon={<DeleteOutlined />} />
+      <Popconfirm
+        title="Delete user?"
+        cancelText="No"
+        okText="Yes"
+        visible={visible}
+        onConfirm={handleOk}
+        onCancel={() => {
+          setVisible(false)
+        }}>
+        <Button
+          size="small"
+          type="text"
+          danger
+          icon={<DeleteOutlined />}
+          onClick={() => {
+            setVisible(!visible)
+          }}
+        />
+      </Popconfirm>
     </Box>
   )
 }
-
-export default CalendarAdvancedView
