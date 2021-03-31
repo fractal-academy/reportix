a51f7e2a13a088f272029b62542137a19db848fb
Viacheslav Shamreliuk <viacheslav@Viacheslavs-MacBook-Pro.local>
2021-03-31 12:02:40 +0300

    added theme

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,19 @@
 const CracoAlias = require('craco-alias')
 
+// module.exports = {
+//   plugins: [
+//     {
+//       plugin: CracoAlias,
+//       options: {
+//         source: 'jsconfig',
+//         baseUrl: './src'
+//       }
+//     }
+//   ]
+// }
+
+const CracoLessPlugin = require('craco-less')
+
 module.exports = {
   plugins: [
     {
@@ -7,6 +21,21 @@ module.exports = {
       options: {
         source: 'jsconfig',
         baseUrl: './src'
+      }
+    },
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              '@primary-color': '#766bd9',
+              '@font-family': '"Montserrat", sans-serif',
+              '@border-radius-base': '6px'
+            },
+            javascriptEnabled: true
+          }
+        }
       }
     }
   ]
